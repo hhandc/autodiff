25a4245304374d7244669767e9b758cdc960d738
hhandc <hyeonhong.chang@gmail.com>
2024-12-08 13:21:17 +0900

    computing adjoint explanation

--- autodiff/ast.py
+++ autodiff/ast.py
@@ -22,6 +22,8 @@ class Node:
                C   D
         B.backward() should be receiving A's adjoint value as the argument adjoint. Then, B.backward() should do the following:
         1. Compute adjoint * dB/dC and pass it as the adjoint for C.backward()
+            a value is evaluated in C when computing dB/dC, which is dA/dB * dB/dC
         2. Compute adjoint * dB/dD and pass it as the adjoint for D.backward()
+            a value is evaluated in D when computing dB/dD, which is dA/dB * dB/dD
         """
         raise NotImplementedError()
