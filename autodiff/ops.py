4e188beeb1aef74da299451158425628102b36aa
hhandc <hyeonhong.chang@gmail.com>
2024-12-08 13:21:17 +0900

    Add more ops and tests

--- autodiff/ops.py
+++ autodiff/ops.py
@@ -8,43 +8,43 @@ class EvalOverloader:
     """
     def __add__(self, other):
         match other:
-            case int(val):
-                return Add(self, Const(other))
+            case int(val) | float(val):
+                return Add(self, Const(val))
             case _:
                 return Add(self, other)
 
     def __radd__(self, other):
         match other:
-            case int(val):
-                return Add(Const(other), self)
+            case int(val) | float(val):
+                return Add(Const(val), self)
             case _:
                 return Add(other, self)
 
     def __sub__(self, other):
         match other:
-            case int(val):
-                return Sub(self, Const(other))
+            case int(val) | float(val):
+                return Sub(self, Const(val))
             case _:
                 return Sub(self, other)
 
     def __rsub__(self, other):
         match other:
-            case int(val):
-                return Sub(Const(other), self )
+            case int(val) | float(val):
+                return Sub(Const(val), self )
             case _:
                 return Sub(self, other)
 
     def __mul__(self, other):
         match other:
-            case int(val):
-                return Mul(self, Const(other))
+            case int(val) | float(val):
+                return Mul(self, Const(val))
             case _:
                 return Mul(self, other)
 
     def __rmul__(self, other):
         match other:
-            case int(val):
-                return Mul(Const(other), self)
+            case int(val) | float(val):
+                return Mul(Const(val), self)
             case _:
                 return Mul(other, self)
 
@@ -53,34 +53,34 @@ class EvalOverloader:
         standard division
         """
         match other:
-            case int(val):
-                return Div(self, Const(other))
+            case int(val) | float(val):
+                return Div(self, Const(val))
             case _:
                 return Div(self, other)
 
     def __rtruediv__(self, other):
         match other:
-            case int(val):
-                return Div(Const(other), self)
+            case int(val) | float(val):
+                return Div(Const(val), self)
             case _:
                 return Div(other, self)
 
     def __pow__(self, other):
         match other:
-            case int(val):
-                return Pow(self, Const(other))
+            case int(val) | float(val):
+                return Pow(self, Const(val))
             case _:
                 return Pow(self, other)
 
     def __rpow__(self, other):
         match other:
-            case int(val):
-                return Pow(Const(other), self)
+            case int(val) | float(val):
+                return Pow(Const(val), self)
             case _:
                 return Pow(other, self)
 
     def __neg__(self):
-        raise NotImplementedError()
+        return Neg(self)
 
 
 class Const(Node, EvalOverloader):
@@ -97,6 +97,9 @@ class Const(Node, EvalOverloader):
     def backward(self, adjoint: float = None):
         return
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.value})"
+
 
 class Op(Node, EvalOverloader):
     """
@@ -104,6 +107,12 @@ class Op(Node, EvalOverloader):
     """
     def __init__(self):
         super().__init__()
+
+    def backward(self, adjoint: float = None):
+        if adjoint is not None:
+            self.adjoint = adjoint
+        else:
+            self.adjoint = 1.0
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}"
@@ -115,15 +124,17 @@ class BinaryOp(Op, EvalOverloader):
         self.left = left
         self.right = right
 
-    def backward(self, adjoint: float = None):
-        if adjoint is not None:
-            self.adjoint = adjoint
-        else:
-            self.adjoint = 1.0
-
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.left}, {self.right})"
 
+
+class UnaryOp(Op, EvalOverloader):
+    def __init__(self, operand: Node):
+        super().__init__()
+        self.operand = operand
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.operand})"
 
 class Add(BinaryOp):
     def eval(self) -> float:
@@ -191,6 +202,18 @@ class Div(BinaryOp):
         self.value = self.left.eval() / self.right.eval()
         return self.value
 
+    def backward(self, adjoint: float = None):
+        """
+        Let self = left / right
+        dself/dleft = adjoint * right^(-1)
+        dself/dright = adjoint * -left
+        """
+        super().backward(adjoint)
+
+        self.left.backward(self.adjoint * self.right.value ** -1)
+
+        self.right.backward(self.adjoint * -self.left.value)
+
 
 class Pow(BinaryOp):
     def eval(self) -> float:
@@ -213,3 +236,45 @@ class Pow(BinaryOp):
 
         # dself/dright = left ** right ln left
         self.right.backward(self.adjoint * self.left.value ** self.right.value * math.log(self.left.value) if self.left.value != 0 else 0)
+
+
+class Cos(UnaryOp):
+    def eval(self) -> float:
+        self.value = math.cos(self.operand.eval())
+        return self.value
+
+    def backward(self, adjoint: float = None):
+        super().backward(adjoint)
+
+        self.operand.backward(self.adjoint * -math.sin(self.operand.value))
+
+class Sin(UnaryOp):
+    def eval(self) -> float:
+        self.value = math.sin(self.operand.eval())
+        return self.value
+    
+    def backward(self, adjoint: float = None):
+        super().backward(adjoint)
+
+        self.operand.backward(self.adjoint * math.cos(self.operand.value))
+
+
+class Exp(UnaryOp):
+    def eval(self) -> float:
+        self.value = math.exp(self.operand.eval())
+        return self.value
+
+    def backward(self, adjoint: float = None):
+        super().backward(adjoint)
+
+        self.operand.backward(self.adjoint * math.exp(self.operand.value))
+
+class Neg(UnaryOp):
+    def eval(self) -> float:
+        self.value = -self.operand.eval()
+        return self.value
+
+    def backward(self,adjoint: float = None):
+        super().backward(adjoint)
+
+        self.operand.backward(-self.adjoint)
--- tests/unit/test_backward.py
+++ tests/unit/test_backward.py
@@ -2,6 +2,7 @@ from autodiff.variable import Variable
 from autodiff.ops import *
 from pytest import approx
 import random
+import math
 
 def test_backward_rosenbrock():
     """
@@ -21,7 +22,6 @@ def test_backward_rosenbrock():
     y = Variable("y", 1)
 
     expr = (a - x) ** 2 + b * (y - x ** 2) ** 2
-    print(expr)
     expr.eval()
     expr.backward()
 
@@ -29,3 +29,36 @@ def test_backward_rosenbrock():
     grad_y = 2 * b * y.value - 2 * b * x.value ** 2
 
     assert x.adjoint == grad_x and y.adjoint == grad_y
+
+def test_backward_matyas():
+    x = Variable("x", 0)
+    y = Variable("y", 0)
+
+    expr = 0.26 * (x**2 + y**2) - 0.48 * x * y
+
+    expr.eval()
+    expr.backward()
+
+    assert expr.value == 0 and x.adjoint == 0 and y.adjoint == 0
+
+def test_backward_schaffer2():
+    x = Variable("x", 0)
+    y = Variable("y", 0)
+
+    expr = 0.5 + (Sin(x ** 2 - y ** 2) ** 2 - 0.5) / (1 + 0.001 * (x ** 2 + y ** 2)) ** 2
+
+    expr.eval()
+    expr.backward()
+
+    assert expr.value == approx(0) and x.adjoint == approx(0) and y.adjoint == approx(0)
+
+def test_backward_easom():
+    x = Variable("x", math.pi)
+    y = Variable("y", math.pi)
+
+    expr = -Cos(x) * Cos(y) * Exp(-((x - math.pi) ** 2 + (y - math.pi) ** 2))
+
+    expr.eval()
+    expr.backward()
+
+    assert expr.value == approx(-1) and x.adjoint == approx(0) and y.adjoint == approx(0)
